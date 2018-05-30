2aa6af99798bb51e968f09daa6641506c04e8ed0
phyxolog <yzhivaga@gmail.com>
2018-05-30 16:04:50 +0300

    Add primitive LinkedList class

--- src/linked_list/main.cpp
+++ src/linked_list/main.cpp
@@ -20,8 +20,47 @@ public:
   Node(T value, Node *p) : value(value), next(p) {}
 };
 
+template <class T> class LinkedList {
+private:
+  Node<T> *head;
+  int size = 0;
+
+public:
+  void Add(T value) {
+    Node<T> *node = new Node<T>(value);
+
+    if (node) {
+      node->next = head;
+      head = node;
+      size++;
+    }
+  }
+
+  void Print() {
+    if (head != nullptr) {
+      Node<T> *n = head;
+
+      while (n) {
+        cout << n->getValue() << endl;
+        n = n->next;
+      }
+    }
+  }
+
+  int getSize() {
+    return size;
+  }
+};
+
 int main(int argc, char *argv[]) {
-  Node<int> *n = new Node<int>(15);
-  cout << n->getValue() << endl;
+  LinkedList<int> *list = new LinkedList<int>;
+  list->Add(15);
+  list->Add(21);
+  list->Add(30);
+
+  cout << "List size: " << list->getSize() << endl << endl;
+
+  list->Print();
+
   return 0;
 }
