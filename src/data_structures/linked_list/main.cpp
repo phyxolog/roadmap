34bb25329311c64d35d8e6744d34e2938555b115
phyxolog <yzhivaga@gmail.com>
2018-05-31 12:49:47 +0300

    Figured out (2)

--- src/data_structures/linked_list/main.cpp
+++ src/data_structures/linked_list/main.cpp
@@ -21,20 +21,23 @@ public:
 
 template <class T> class LinkedList {
 private:
-  Node<T> *head;
-  Node<T> *tail;
+  Node<T> *head; // pointer to first node
+  Node<T> *tail; // pointer to last node
   int size = 0;
 
 public:
   void PushBack(T value) {
+    // first off create new node.
     Node<T> *node = new Node<T>(value);
 
-    if (node != nullptr) {
+    // if memory allocated
+    if (node != nullptr) {      
+      // then check if head != null
       if (head != nullptr) {
         tail->next = node;
         node->prev = tail;
         tail = node;
-      } else {
+      } else {        
         head = tail = node;
       }
 
@@ -50,7 +53,7 @@ public:
         head->prev = node;
         node->next = head;
         head = node;
-      } else {
+      } else {        
         head = tail = node;
       }
 
@@ -67,6 +70,7 @@ public:
   }
 
   void Print() {
+    // start printing from head
     if (head != nullptr) {
       Node<T> *n = head;
 
@@ -79,18 +83,42 @@ public:
 
   T PopFront() {
     if (head != nullptr) {
+      // remember a value
       T temp = head->getValue();
+
+      // get next node and delete current
       Node<T> *n = head->next;
       delete head;
+
+      // decrement list size
       size--;
 
       if (n != nullptr) {
         head = n;
         head->prev = nullptr;
       } else {
-        head = nullptr;
+        head = tail = nullptr;
       }
 
+      return temp;
+    }
+  }
+
+  T PopBack() {
+    // similar to PopFront, but work from the end
+    if (tail != nullptr) {
+      T temp = tail->getValue();
+      Node<T> *n = tail->prev;
+      delete tail;
+      size--;
+
+      if (n != nullptr) {
+        tail = n;
+        n->next = nullptr;
+      } else {
+        head = tail = nullptr;
+      }
+      
       return temp;
     }
   }
@@ -110,34 +138,28 @@ int main(int argc, char *argv[]) {
   LinkedList<int> *list = new LinkedList<int>;
 
   list->PushBack(15);
-  list->PushBack(21);
+  list->PushFront(21);
   list->PushBack(30);
 
-  list->PopFront();
+  cout << "Pops: " << endl;
+  cout << list->PopBack() << endl; // 30
+  cout << list->PopBack() << endl; // 15
+  cout << list->PopBack() << endl; // 21
+  cout << list->PopBack() << endl << endl; // 0 because now list was empty
 
-  cout << list->getHead()->getValue() << endl;
+  list->PushBack(43);
+  list->PushBack(42);
+  list->PushBack(41);
 
-  std::list<int> vec;
-  vec.push_back(15);
-  vec.push_back(21);
-  vec.push_back(30);
+  cout << "Tail: " << list->getTail()->getValue() << endl << endl; // 41
 
-  vec.pop_front();
+  cout << "List size: " << list->getSize() << endl << endl; // 3
 
-  cout << vec.front() << endl;
+  list->Print();
 
-  // cout <<  << endl;
+  list->~LinkedList();
 
-
-  // cout << "List size: " << list->getSize() << endl << endl;
-
-  // list->Print();
-
-  // cout << list->getHead()->next->getValue() << endl;
-
-  // list->~LinkedList();
-
-  // cout << endl << "List size: " << list->getSize() << endl << endl;
+  cout << endl << "List size: " << list->getSize() << endl << endl;
 
   return 0;
 }
