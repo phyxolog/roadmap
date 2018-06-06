3eb14947ea2504e176ffc94de48a798cb97116ff
phyxolog <yzhivaga@gmail.com>
2018-06-06 13:53:19 +0300

    [HashMap] Step 1. Create class.

--- src/data_structures/hash_map.cpp
+++ src/data_structures/hash_map.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 template<typename K, typename V>
-class Node {
+class Entry {
 private:
   K key;
   V value;
 
 public:
-  Node(K key, V value) : key(key), value(value) {}
+  Entry *next;
+  Entry(K key, V value, Entry next = nullptr) : key(key), value(value), next(next) {}
 
   K getKey() {
     return key;
@@ -20,6 +21,37 @@ public:
   }
 };
 
+template<typename K, typename V>
+class HashMap {
+private:
+  Entry<K, V> **table;
+  int size = 0;
+  int capacity = 16;
+
+public:
+  HashMap() {
+    table = new Entry<K, V> *[capacity]();
+  }
+
+  unsigned long hashFunc(K key) {
+    unsigned long hash = (unsigned long)(key);
+    hash ^= (hash >> 20) ^ (hash >> 12);
+    return hash ^ (hash >> 7) ^ (hash >> 4);
+  }
+
+  int indexFor(unsigned long hash, int tableLength) {
+    return hash & (tableLength - 1);
+  }
+
+  void put(K key, V value) {
+    unsigned long hash = hashFunc(key);
+    cout << hash << endl;
+  }
+};
+
 int main(int argc, char *argv[]) {
+  HashMap<int, string> *map = new HashMap<int, string>;
+  map->put(51, "1");
+
   return 0;
 }
