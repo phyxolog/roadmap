2bacbe3eed448d3fa217bf3021a33c31392912d1
phyxolog <yzhivaga@gmail.com>
2018-06-11 14:06:20 +0300

    Tried to understand hashmap.

--- src/data_structures/hash_map.cpp
+++ src/data_structures/hash_map.cpp
@@ -10,7 +10,8 @@ private:
 
 public:
   Entry *next;
-  Entry(K key, V value, Entry next = nullptr) : key(key), value(value), next(next) {}
+  Entry(const K &key, const V &value, Entry *next = nullptr)
+    : key(key), value(value), next(next) {}
 
   K getKey() {
     return key;
@@ -18,6 +19,14 @@ public:
 
   V getValue() {
     return value;
+  }
+
+  void setKey(K key) {
+    Entry::key = key;
+  }
+
+  void setValue(V value) {
+    Entry::value = value;
   }
 };
 
@@ -34,24 +43,90 @@ public:
   }
 
   unsigned long hashFunc(K key) {
-    unsigned long hash = (unsigned long)(key);
-    hash ^= (hash >> 20) ^ (hash >> 12);
-    return hash ^ (hash >> 7) ^ (hash >> 4);
+    return key % capacity;
   }
 
-  int indexFor(unsigned long hash, int tableLength) {
-    return hash & (tableLength - 1);
-  }
-
-  void put(K key, V value) {
+  V get(const K &key) {
     unsigned long hash = hashFunc(key);
-    cout << hash << endl;
+    Entry<K, V> *entry = table[hash];
+
+    while (entry != nullptr) {
+      if (entry->getKey() == key) {
+        return entry->getValue();
+      }
+
+      entry = entry->next;
+    }
+
+    return V();
+  }
+
+  void put(const K &key, const V &value) {
+    unsigned long hash = hashFunc(key);
+    Entry<K, V> *prev = nullptr;
+    Entry<K, V> *entry = table[hash];
+
+    if (size >= capacity) {
+      resize(capacity * 2);
+    }
+
+    while (entry != nullptr && entry->getKey() != key) {
+      prev = entry;
+      entry = entry->next;
+    }
+
+    if (entry == nullptr) {
+      entry = new Entry<K, V>(key, value);
+
+      if (prev == nullptr) {
+        table[hash] = entry;
+      } else {
+        prev->next = entry;
+      }
+
+      size++;
+    } else {
+      entry->setValue(value);
+    }
+  }
+
+  void resize(newCapacity) {
+    // TODO
+  }
+
+  void del(const K &key) {
+    unsigned long hash = hashFunc(key);
+    Entry<K, V> *prev = nullptr;
+    Entry<K, V> *entry = table[hash];
+
+    while (entry != nullptr && entry->getKey() != key) {
+      prev = entry;
+      entry = entry->next;
+    }
+
+    if (entry != nullptr) {
+      if (prev == nullptr) {
+        table[hash] = entry->next;
+      } else {
+        prev->next = entry->next;
+      }
+
+      size--;
+      delete entry;
+    }
   }
 };
 
 int main(int argc, char *argv[]) {
   HashMap<int, string> *map = new HashMap<int, string>;
-  map->put(51, "1");
+  map->put(1, "1");
+  map->put(1, "12");
+  
+  cout << map->get(1) << endl; // 12
+
+  map->del(1);
+
+  cout << map->get(1) << endl; // null
 
   return 0;
 }
